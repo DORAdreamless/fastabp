d8b0d23e8be5ddeac8e78af706a2865801df5882
哆啦A梦 <460247986@qq.com>
2025-06-29 12:48:04 +0800

    添加文件存储实体

---
+++ src/HB.AbpFundation.Domain/AggregateRoot/ObjectStorage/FileStorage.cs
@@ -0,0 +1,46 @@
+﻿using HB.AbpFundation.Helpers;
+using HB.AbpFundation.Persistences;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.AbpFundation.AggregateRoot.ObjectStorage
+{
+    /// <summary>
+    /// 文件存储
+    /// </summary>
+    [Table("storage_file_storage")]
+    public class FileStorage: PersistenceObjectBase
+    {
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        [StringLength(100)]
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        [StringLength(100)]
+        public string FilePath { get; set; }
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        public FileType FileType { get; set; }
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long FileSize { get; set; }
+        /// <summary>
+        /// 文件存储提供者（阿里云对象存储，Minio对象存储，七牛云对象存储）
+        /// </summary>
+        [StringLength(50)]
+        public string FileProvider { get; set; }
+    }
+
+    
+}
